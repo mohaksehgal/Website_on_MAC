551162996648e860d8ced41894363c8493d5afc5
mohaksehgal <jsmohak@gmail.com>
2021-11-24 16:47:38 +0530

    app name change

--- EssenciaWebsite/settings.py
+++ EssenciaWebsite/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'essencia-website.herokuapp.com', 'websitedeployme
 
 INSTALLED_APPS = [
     'import_export',
-    'FirstLevel',
+    'FirstLevel.apps.FirstlevelConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
