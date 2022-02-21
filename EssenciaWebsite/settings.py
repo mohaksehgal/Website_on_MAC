9ad3cb531185eb772d8bad3a354c1587fde8d71b
mohaksehgal <jsmohak@gmail.com>
2022-02-21 23:42:13 +0530

    Checking2

--- EssenciaWebsite/settings.py
+++ EssenciaWebsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0+68d^*m_8qk2iu3&5hk1kgyjwmkolt_&0we-f%pl59y21c)sz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'essencia-website.herokuapp.com', 'websitedeployment1.herokuapp.com','*']
 
